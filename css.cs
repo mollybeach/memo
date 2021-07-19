58c68b2f1b0610175c55ae6d342f3bcafd802b5c
Molly Beach <mollyjbeach@gmail.com>
2021-07-19 14:28:08 -0700

    Update and rename css.md to css.cs

--- css.cs
+++ css.cs
@@ -1,12 +1,12 @@
 
 
-below 500px display nothing 
+/*below 500px display nothing */
 @media (max-width: 500px ) {
   .hide-3 {
     display: none;
   }
 }
-above 500px display nothing
+/*above 500px display nothing*/
 @media (min-width: 500px ) {
   .hide-3 {
     display: none;
