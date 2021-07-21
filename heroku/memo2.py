b70312e3606b2c5c72a6fc8ecc8c5cac78d167c5
Molly Beach <mollyjbeach@gmail.com>
2021-07-20 20:17:27 -0700

    Update memo2.py

--- heroku/memo2.py
+++ heroku/memo2.py
@@ -20,4 +20,17 @@
 #Anyway when you have DB you need to connect your APP with DB on Heroku. For this purpose I added in settings.py:
 
 #DATABASES = {'default': dj_database_url.config(default=‘URI of DB on Heroku’)}
+
+#database not loading ?
+#You've not set a "real" production database
+#You've not used rake db:migrate in production
+#heroku run rake db:migrate
+
+#Firstly, you need to create a production db to run on Heroku. Heroku does not host any db for you - it only runs on the Amazon EC2 stack; meaning you have to either prodivde your own DB server, or use one of Heroku's postgres instances
+
+#After you have a production database, you'll just need to run heroku run rake db:migrate from your command-line to get all your tables populated on the db server, which should resolve the error
+
+
 #heroku run python manage.py runserver
+
+
